98399d97cbb0a7104ab4fc7f7930c53388c63033
Balakrishnan255 <balakrishnan3706@gmail.com>
2026-01-03 18:24:30 +0530

    Add files via upload
    project uploaded

---
+++ script.js
@@ -0,0 +1,13 @@
+function addTask() {
+    let taskInput = document.getElementById("taskInput");
+    let taskList = document.getElementById("taskList");
+
+    if (taskInput.value === "") return;
+
+    let li = document.createElement("li");
+    li.textContent = taskInput.value;
+    li.onclick = () => li.remove();
+
+    taskList.appendChild(li);
+    taskInput.value = "";
+}
